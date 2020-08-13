f397639653666ea1dfa3557ec3f3f455b7bee048
Harry Peralta <alba45278@gmail.com>
2020-08-13 07:38:32 -0500

    Archivos indice

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+    // phpinfo();
+    echo "Hola ", "mundo ", "PHP ", 7; 
+    print 'Hola mundo';
+    print 'Hola mundo '. 'PHP';
+    echo '<p>Hola mundo </p>';
+    echo '<p> esto es PHP</p>';
+
+echo'<br></br>';
+echo "=========================";
+echo"Operadores aritmeticos"; 
+echo "=========================";
+echo'<br></br>';
+
+$x = 8;
+$y = 4;
+
+
+?>
